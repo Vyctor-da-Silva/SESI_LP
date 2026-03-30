86114fc9dac5176d21c4d80920b23b1409587cd3
vyctor da silva <vyctoriesbik@gmail.com>
2026-03-30 11:55:15 -0300

    Aula 01 C

---
+++ aula1/EX_03.C
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+	float n1, n2, n3, media;
+
+	printf("qual a n1:");
+	scanf("%f", &n1);
+
+	printf("qual a n2:");
+	scanf("%f", &n2);
+
+	printf("qual a n3:");
+	scanf("%f", &n3);
+
+	media = ("(%.2f + %.2f + %.2f) / %.2f", (n1 + n2 + n3) / 3);
+
+	printf("sua media %.2f C):", media);
+
+	if(media >= 7.0) {
+		printf("APROVADO");
+	} else if(media <= 2.9) {
+		printf("REPROVADO");
+	} else {
+		printf("RECUPERACAO");
+	}
+
+
+}
